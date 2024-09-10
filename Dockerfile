b8e2dea12e93bbbd963342a94be8751bdfc91dd0
Oleg Lobanov <oleg.lobanov@bitvavo.com>
2024-09-10 15:48:03 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Use an official Python runtime as a parent image
+FROM python:3.12-slim
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY main.py main.py
+
+ENV TOKEN=""
+ENV CHAT_ID=""
+
+# Run the Python script when the container launches
+CMD ["python", "./main.py"]
---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import telepot
+import time
+import os
+
+url = 'https://reg.devoxx.be/api/v2/public/event/2024/ticket-categories'
+
+bot = telepot.Bot(os.getenv("TOKEN"))
+chat_id = os.getenv("CHAT_ID")
+
+
+def check_tickets():
+    try:
+        response = requests.get(url).json()
+
+        ticket_categories = response.get("ticketCategories", [])
+
+        target_category_id = 46
+
+        for category in ticket_categories:
+            if category.get("id") == target_category_id:
+                maximum_saleable_tickets = category.get("maximumSaleableTickets")
+                if maximum_saleable_tickets > 0:
+                    message = f"Category ID {target_category_id} has {maximum_saleable_tickets} tickets available!"
+                    bot.sendMessage(chat_id, message)
+                    print("Notification sent to Telegram")
+                else:
+                    print("No tickets available for category ID 46")
+                break
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+
+if __name__ == '__main__':
+    while True:
+        check_tickets()
+        time.sleep(10)
